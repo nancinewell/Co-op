bd71d0a22e77d4b2ef99faac2c691476fd7daedc
nancinewell <nanci.newell@gmail.com>
2024-08-04 20:29:13 -0500

    update

--- js/miracle.js
+++ js/miracle.js
@@ -22,13 +22,28 @@ function miracle(){
 
 function randomEvent(){
     let set = [
-        "Costume Party! A party without costumes just isn't a party! Everyone must wear a distinctive accessory.",
-        "Dance Party! You get 1 minute to dance your pants off! (Please don't really take your pants off!)",
-        "You had a great night's sleep! You're energized for the day. Receive 2 mana.",
-        "You had a nightmare! You didn't get good sleep because of it. Lose 1 mana.",
-        "You're practicing your courtly manners for the king's visit. Call everyone Milord and Milady for the rest of class.",
-        "Gift of the game master! The teacher gives a surprise to all players!",
-        "The sky is the limit! ...Unless you're playing indoors. Everyone has 30 seconds to make a paper airplane. The furthest to fly wins!"
+        "Costume Party! A party without costumes just isn't a party! Everyone must wear a distinctive accessory. Everyone gains 1 SP.",
+        "Dance Party! You get 1 minute to dance your pants off! (Please don't really take your pants off!) Everyone gains 1 SP.",
+        "You had a great night's sleep! You're energized for the day.  Everyone gains 2 SP..",
+        "You had a nightmare! You didn't get good sleep because of it. Everyone lose 1 SP.",
+        "You're practicing your courtly manners for the king's visit. Call everyone Milord and Milady for the rest of class. Everyone gains 1 SP.",
+        "Gift of the game master! The teacher gives a surprise to all players! Everyone gains 1 SP.",
+        "The sky is the limit! ...Unless you're playing indoors. Everyone has 30 seconds to make a paper airplane. The furthest to fly wins 100XP!  Everyone gains 1 SP.",
+        "Ahoy, Mayeys! Ye best be seafaring folk, or ye will walk the plank! Everyone who speaks with a pirate accent for the class earns 150XP. Everyone gains 1 SP.",
+        "A magical storm blocks the SP regeneration for all but the most powerful. Only the highest level player(s) gains 1 SP.",
+        "An angel decides to help the feeble. The player with the least XP receives 300XP. Everyone gains 1 SP.",
+        "You find a potion and give it to your weakest teammate. The player with the fewest SP receives 2 SP. Everyone gains 1 SP.",
+        "We see it once in a year, twice in a week, and never in a day. What is it? ...The letter 'E'! The teacher picks 6 letters. Each team must come up with as many words as possible using only those letters in 30 seconds.The team with the most words gains 250XP. Everyone gains 1 SP.",
+        "Amazon Prime would like you to review an item you purchased. One random player stands in front of the board, facing away from it. All other students come and write one truthful compliment about that student on the board behind him or her. The teacher will take a picture of the board before you sit down. Everyone gains 1 SP.",
+        "An angel visits the healers and bestows her powers upon them. All healers gain 1 SP & 1 HP Everyone gains 1 SP.",
+        "You spot a bandit camp just off the trail up ahead. They haven't spotted your team yet, but you'll need to be careful to get past them... Or you could try to sneak in and steal some gold as you pass. One random player rolls a D20. 1-6: You're spotted! Your whole team loses 3HP. 7-14: You sneak past. Your whole team gains 100XP. 15-20: While sneaking past, your team relieves the bandits of some of their plunder. Your whole team receives 100XP and 50GP. Everyone gains 1 SP."/*,
+        " Everyone gains 1 SP.",
+        " Everyone gains 1 SP.",
+        " Everyone gains 1 SP.",
+        " Everyone gains 1 SP.",
+        " Everyone gains 1 SP.",
+        " Everyone gains 1 SP.",
+        " Everyone gains 1 SP.",*/
     ];
 
     let random = Math.floor(Math.random()*(set.length-1));
